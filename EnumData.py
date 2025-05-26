e275092138f7514c55384265eadc176de778e1e1
apdlover <30372323+apdlover@users.noreply.github.com>
2025-05-26 22:10:51 +0800

    Initial commit

---
+++ EnumData.py
@@ -0,0 +1,10 @@
+class OriginalNameUnified:
+    column_mapping_akshare = {
+        '日期': 'Date',
+        '开盘': 'Open',
+        '收盘': 'Close',
+        '最高': 'High',
+        '最低': 'Low',
+        '成交量': 'Volume',
+        '成交额': 'Amount',
+    }
